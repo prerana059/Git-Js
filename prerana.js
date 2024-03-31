820296c1bf93031abbb48651bffb874ea760ec97
Prerana <prebhme@gmail.com>
2024-03-31 21:04:31 +0545

    first commit

---
+++ prerana.js
@@ -0,0 +1,26 @@
+function diff(a,b){
+    return a-b;
+
+}
+
+console.log(diff(2,3))
+ 
+
+
+const diff2 = (a,b)=> {
+    console.log(a,b)
+    return a-b
+};
+
+const diff1 =(a,b,c)=>{
+    console.log('doing something cool')
+    return a-b-c;
+}
+
+const sayHello =() => {
+    console.log("hello")
+}
+
+a='2'
+b=2
+
